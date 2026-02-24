a51c3f79bbb280b13ce130178bf2fb421e760e02
ariellalgilmore <ariellalgilmore@gmail.com>
2026-02-24 11:21:40 -0800

    update index file

--- packages/web-components/src/components/accordion/index.ts
+++ packages/web-components/src/components/accordion/index.ts
@@ -1,11 +1,14 @@
 /**
- * Copyright IBM Corp. 2021, 2022, 2023
+ * Copyright IBM Corp. 2021, 2026
  *
  * This source code is licensed under the Apache-2.0 license found in the
  * LICENSE file in the root directory of this source tree.
  */
 
-import './accordion';
-import './accordion-item';
-import './accordion-item-skeleton';
-import './accordion-skeleton';
+import { registerGlobal } from '../../globals/register';
+import CDSAccordion from './accordion';
+import CDSAccordionItem from './accordion-item';
+import CDSAccordionItemSkeleton from './accordion-item-skeleton';
+import CDSAccordionSkeleton from './accordion-skeleton';
+
+registerGlobal(CDSAccordion, CDSAccordionItem, CDSAccordionItemSkeleton, CDSAccordionSkeleton);
